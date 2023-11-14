85caac5c6e0a2a51cbe4b6158190019213cc3925
Juliet <musyoki.juliet011@gmail.com>
2023-11-14 15:29:10 +0300

    DONE

--- contact-manager/src/Navbar.js
+++ contact-manager/src/Navbar.js
@@ -8,7 +8,7 @@ const NavigationBar = () => {
     <Navbar bg="light" expand="lg">
       <Navbar.Collapse id="navbarScroll">
         <Nav className="me-auto" style={{ maxHeight: '100px' }} navbarScroll>
-          <Nav.Link as={Link} to="/">Home</Nav.Link>
+          <Nav.Link as={Link} to="/">List</Nav.Link>
           <Nav.Link as={Link} to="/add-contact">Add Contact</Nav.Link>
         </Nav>
       </Navbar.Collapse>
